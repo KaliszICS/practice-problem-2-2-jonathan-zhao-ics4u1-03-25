71df7a78c9c4b5f5fcac4902335fb83864a8c5de
jonathan-zhao-ics4u1-03-25 <jonathan.zhao@student.tdsb.on.ca>
2025-03-19 18:50:55 +0000

    Done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,72 @@
+import java.util.Arrays;
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 		
 	}
 	
-}
+	public static int[] compareSearch(int[] arr, int number){
+		Arrays.sort(arr);
+		int[] count = new int[] {0, 0};
+		int counterSeq = 0;
+		int counterBin = 0;
+			for (int s = 0; s < arr.length; s++){
+				counterSeq++;
+				if (number == arr[s]){
+					break;
+			}
+		}
+			int low =0;
+			int high = arr.length-1;
+			int mid;
+				while (low <= high){
+				mid = (low + high) / 2;
+				counterBin++;
+			if (number == arr[mid]){
+			low = high+1;
+		} else if (number > arr[mid]) {
+			low = mid + 1;
+		} else {
+			high = mid-1;
+		}
+	}
+	count[0] = counterSeq;
+	count[1] = counterBin;
+	return count;
+	}
+
+	public static int[] compareStringSearch(String[] arr, String str){
+		Arrays.sort(arr);
+		int[] count = new int[] {0, 0};
+		int counterSeq = 0;
+		int counterBin = 0;
+		for (int s = 0; s<arr.length; s++){
+		counterSeq++;
+		if (str == arr[s]){
+			s = arr.length;
+		}
+		}
+		int low =0;
+		int high = arr.length-1;
+		int mid;
+		while (low<=high){
+			mid = (low+high)/2;
+			counterBin++;
+			if (str == arr[mid]){
+				low = high+1;
+			}
+			else if (str.compareTo(arr[mid])>0){
+				low = mid+1;
+			}
+			else{
+				high = mid-1;
+			}
+		}
+		
+		count[0] = counterSeq;
+		count[1] = counterBin;
+		return count;
+		}
+	}
+
